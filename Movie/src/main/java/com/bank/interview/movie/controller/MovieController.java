12f43f468e9d7d59e0e055e4b0f783bc10547645
tuannguyenducanh <nguyenducanhtuan1011@gmail.com>
2023-10-24 22:24:52 +0700

    Initialize project

---
+++ Movie/src/main/java/com/bank/interview/movie/controller/MovieController.java
@@ -0,0 +1,4 @@
+package com.bank.interview.movie.controller;
+
+public class MovieController {
+}
---
+++ mysql_ds/init-scripts/movie.sql
@@ -0,0 +1,7 @@
+CREATE TABLE movie (
+  id bigint(20) AUTO_INCREMENT NOT NULL,
+  title varchar(50) NOT NULL,
+  category varchar(50) NOT NULL,
+  rating varchar(255) NOT NULL,
+   PRIMARY KEY (id)
+);
